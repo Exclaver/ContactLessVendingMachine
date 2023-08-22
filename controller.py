5417adb66ffeeeccff37c45291da693eeb6a8a11
Devansh <devansh0403@gmail.com>
2023-08-22 11:58:11 +0530

    version4.1

--- controller.py
+++ controller.py
@@ -1,48 +1,18 @@
 import pyfirmata
-comport='COM7'
-board=pyfirmata.Arduino(comport)
+port='COM7'
+board=pyfirmata.Arduino(port)
 
-led_1=board.get_pin('d:13:o')
-led_2=board.get_pin('d:12:o')
-led_3=board.get_pin('d:11:o')
-led_4=board.get_pin('d:10:o')
-led_5=board.get_pin('d:9:o')
+def rotateServo(pin,angle):
+    board.digital[pin].write(angle)
+    sleep(0.015)
+
+
+
 
 def led(selectionList):
-    if selectionList[0]!=-1 and selectionList[1]!=-1 and selectionList[2]!=-1:
-        if selectionList[0]==1:
-            led_1.write(0)
-            led_2.write(0)
-            led_3.write(0)
-            led_4.write(0)
-            led_5.write(0)
-        elif totalFingers==1:
-            led_1.write(1)
-            led_2.write(0)
-            led_3.write(0)
-            led_4.write(0)
-            led_5.write(0)
-        elif totalFingers==2:
-            led_1.write(1)
-            led_2.write(1)
-            led_3.write(0)
-            led_4.write(0)
-            led_5.write(0)
-        elif totalFingers==3:
-            led_1.write(1)
-            led_2.write(1)
-            led_3.write(1)
-            led_4.write(0)
-            led_5.write(0)
-        elif totalFingers==4:
-            led_1.write(1)
-            led_2.write(1)
-            led_3.write(1)
-            led_4.write(1)
-            led_5.write(0)
-        elif totalFingers==5:
-            led_1.write(1)
-            led_2.write(1)
-            led_3.write(1)
-            led_4.write(1)
-            led_5.write(1)
+    if selectionList[2]==1:
+        for j in range(selectionList[2]):
+                for i in range(0,180):
+                    rotateServo(selectionList[0],360)
+    
+            
